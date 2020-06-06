5699d7984d5b80170838db37c2165486c7bc4828
victorcabeceira <victorfgcabeceira@gmail.com>
2020-06-06 14:47:04 -0300

    Working with readLine module

---
+++ readLineModule/app.js
@@ -0,0 +1,32 @@
+// Prompt the user as well as get user input
+const readline = require('readline');
+
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout
+});
+
+let num1 = Math.floor(Math.random() * 10 + 1);
+let num2 = Math.floor(Math.random() * 10 + 1);
+let answer = num1 + num2;
+
+rl.question(`What is ${num1} + ${num2}?\n`, userInput => {
+  if (userInput.trim() == answer) {
+    rl.close();
+  } else {
+    rl.setPrompt('Incorrect response. Please try again.\n');
+    rl.prompt();
+    rl.on('line', userInput => {
+      if (userInput.trim() == answer) {
+        rl.close();
+      } else {
+        rl.setPrompt(`Your answer of ${userInput} is incorrect, try again\n`);
+        rl.prompt();
+      }
+    });
+  }
+});
+
+rl.on('close', () => {
+  console.log('Correct answer!');
+});
