cf52a5e861df87a0a72810a51845194ee7d1517f
victorcabeceira <victorfgcabeceira@gmail.com>
2020-06-06 17:42:41 -0300

    Input validation pt1

--- httpPostRequest/index.js
+++ httpPostRequest/index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const path = require('path');
 const bodyParser = require('body-parser');
+const Joi = require('joi');
 
 const app = express();
 
@@ -13,9 +14,22 @@ app.get('/', (req, res) => {
 });
 
 app.post('/', (req, res) => {
-  // console.log('req.body', req.body);
+  console.log('req.body', req.body);
+
+  const schema = Joi.object().keys({
+    email: Joi.string().trim().email().required(),
+    password: Joi.string().min(4).max(10).required(),
+  });
+
+  Joi.validate(req.body, schema, (error, result) => {
+    if (error) {
+      console.log('error', error);
+      res.send('An error has occurred');
+    }
+    console.log('result', result);
+    res.send('successfully posted data');
+  });
   // database work here
-  res.json({ success: true });
 });
 
 app.listen(3000);
