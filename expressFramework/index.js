9e9e735cb52d9918f9b77697a2c28ca7c51b8102
victorcabeceira <victorfgcabeceira@gmail.com>
2020-06-06 16:47:55 -0300

    Serving static files with express

--- expressFramework/index.js
+++ expressFramework/index.js
@@ -1,8 +1,12 @@
 const express = require('express');
+const path = require('path');
+
 const app = express();
 
+app.use('/public', express.static(path.join(__dirname, 'static')));
+
 app.get('/', (req, res) => {
-  res.send('Hello world');
+  res.sendFile(path.join(__dirname, 'static', 'index.html'));
 });
 
 app.get('/example', (req, res) => {
