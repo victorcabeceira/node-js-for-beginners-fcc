21f2fa815733d81da47810cd9606a7d0e0cc7307
victorcabeceira <victorfgcabeceira@gmail.com>
2020-06-06 18:32:41 -0300

    Using ejs

---
+++ ejsTemplatesWIthExpress/index.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const path = require('path');
+const app = express();
+
+app.use('/public', express.static(path.join(__dirname, 'static')));
+
+app.set('view engine', 'ejs');
+
+app.get('/:userQuery', (req, res) => {
+  res.render('index', {
+    data: {
+      userQuery: req.params.userQuery,
+      searchResults: ['book1', 'book2', 'book3'],
+      loggedIn: true,
+      userName: 'Victor',
+    },
+  });
+});
+
+app.listen(3000);
